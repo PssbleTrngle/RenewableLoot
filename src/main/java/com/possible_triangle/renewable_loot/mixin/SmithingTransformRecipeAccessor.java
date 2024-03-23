8bf047924297d2e787a60f9b649682e83f285f79
Niklas Widmann <nik@somethingcatchy.net>
2024-03-23 16:52:34 +0100

    smithing nbt support

---
+++ src/main/java/com/possible_triangle/renewable_loot/mixin/SmithingTransformRecipeAccessor.java
@@ -0,0 +1,14 @@
+package com.possible_triangle.renewable_loot.mixin;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.SmithingTransformRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(SmithingTransformRecipe.class)
+public interface SmithingTransformRecipeAccessor {
+
+    @Accessor
+    ItemStack getResult();
+
+}
